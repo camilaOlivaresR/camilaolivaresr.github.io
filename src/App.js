f31122e8efd579d1e01732a6a3f4475a158af464
cami oliva <lamica.olivares@gmail.com>
2025-03-03 14:21:29 -0300

    style and colors

--- src/App.js
+++ src/App.js
@@ -52,18 +52,17 @@ function App() {
         <div class="d-flex justify-content-center ">
 
           <ul class="list-inline ">
+          <span>© 2024 HandMade CamiDev </span>
             <li class="list-inline-item">
-              <span>© 2024 HandMade CamiDev 
                 <a
                 data-mdb-ripple-init
                 class="btn btn-link btn-floating btn-lg text-body "
                 href="https://es.react.dev/"
                 role="button"
                 target="_blank"
-                ><i class="fab fa-react fa-2x"></i> 
+                ><i class="fab fa-react fa-1x"></i> 
 
                 </a>
-              </span>
             </li>
             <li class="list-inline-item">
               <a

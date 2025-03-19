02d48a7560e340aee915b11dce443ffe10c95e07
cami oliva <lamica.olivares@gmail.com>
2025-03-19 15:26:07 -0300

    letter

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,22 @@ import cara from './cara.jpeg'
 const Home = () => {
   return (
     <>
-        <div class="d-flex justify-content-center mb-4">
-        <img src={cara} class="rounded-circle mx-auto d-block" id='responsive' alt="..."/>
-        </div>
-        <div>
+      <div class="d-flex justify-content-center mb-4">
+        <img src={cara} class="rounded-circle mx-auto d-block" id='responsive' alt="..." />
+      </div>
+      <div id='parrafo'>
         <p>
-       Hola! Soy Camila desarrolladora Junior Full-Stack en JavaScript y Ruby. Creo sitios web y hago cosas relacionadas con Internet, tengo experiencia en la creación de sistemas y aplicaciones web responsivas, diseño y creo todo de principio a fin . 
-       </p>
-       <p>
-       Actualmente trabajo de manera Freelace y ocasionalmente estoy disponible para proyectos con un fuerte énfasis en la accesibilidad, la equidad de género, la interseccionalidad, la justicia social y las artes. 
-       </p>
-       <p>
-       Entusiasta por aprender nuevas herramientas para crecer en el campo TI . Motivada por contribuir al éxito del equipo a través del trabajo proactivo, atención a los detalles y buena comunicación. Voluntaria ocasional educando y acompañando a niñas en el descubrimiento de la programación .
-       </p>
-       </div>
-       
-     
+          Hola! Soy Camila desarrolladora Junior Full-Stack en <a>JavaScript - Ruby</a>. Creo sitios web y hago cosas relacionadas con Internet, tengo experiencia en la creación de sistemas y aplicaciones web responsivas, diseño y creo todo de principio a fin .
+        </p>
+        <p>
+          Actualmente trabajo de manera <a>Freelace</a> y ocasionalmente estoy disponible para proyectos con un fuerte énfasis en la accesibilidad, la equidad de género, la interseccionalidad, la justicia social y las artes.
+        </p>
+        <p>
+          Entusiasta por aprender nuevas herramientas para crecer en el campo TI . Motivada por contribuir al éxito del equipo a través del trabajo proactivo, atención a los detalles y buena comunicación. Voluntaria ocasional educando y acompañando a niñas en el descubrimiento de la programación .
+        </p>
+      </div>
+
+
     </>
   )
 }

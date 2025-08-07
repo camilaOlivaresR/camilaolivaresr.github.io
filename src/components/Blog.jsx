4f3fe4b7014c98af88cd6afe8516db02ae64eae0
cami <cami@localhost.localdomain>
2025-08-07 15:48:49 -0400

    json file blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,21 +1,40 @@
-import React from 'react'
-import ada from './img/ada.jpeg'
+import { useEffect, useState } from 'react';
+import ada from './img/ada.jpeg';
+
 
 
 const Blog = () => {
+  const [data, setData] = useState(null);
+
+ useEffect(() => {
+    fetch('/data.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div class="mb-5">
       <div class="d-flex justify-content-center">
         <h2>Blog</h2>
       </div>
       <div class="row">
+         
+        {data.posts.map(post => (
+        
+  
+     
         <div class="col-md-4 p-2" >
           <div class="card h-100">
             <div class="card" >
-              <img src={ada} class="card-img-top" alt="..." />
+              <img src={post.image} class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <h5 class="card-title">key={post.id},</h5>
+                <p class="card-text">{post.title}.</p>
               </div>
 
               <div class="card-body">
@@ -25,47 +44,7 @@ const Blog = () => {
             </div>
           </div>
         </div>
-        <div class="col-md-4 p-2" >
-          <div class="card h-100">
-            {/* <img src="..." class="card-img-top" alt="..." /> */}
-            {/* <div class="card-body">
-              <h5 class="card-title">Portafolio de Fotos
-                <a
-                  data-mdb-ripple-init
-                  class="btn btn-link btn-floating btn-lg text-body m-1"
-                  href="#!"
-                  role="button"
-                  data-mdb-ripple-color="dark"
-                  target="_blank"
-                ><i class="fa fa-cube"></i>
-                </a>
-              </h5>
-              <p class="card-text">Karina pide desarrollar una aplicación simple para que los usuarios puedan ver sus fotografías y comentarlas.</p>
-              <a href="https://github.com/camilaOlivaresR/ProyectoPortafolioFotos" target="_blank" class="btn border">Github</a>
-            </div> */}
-          </div>
-        </div>
-        <div class="col-md-4 p-2" >
-          <div class="card h-100">
-            {/* <img src="..." class="card-img-top" alt="..." /> */}
-            {/* <div class="card-body">
-              <h5 class="card-title">The Rial News
-                <a
-                  data-mdb-ripple-init
-                  class="btn btn-link btn-floating btn-lg text-body m-1"
-                  href="#!"
-                  role="button"
-                  data-mdb-ripple-color="dark"
-                  target="_blank"
-                ><i class="fa fa-cube"></i>
-                </a>
-              </h5>
-              <p class="card-text">
-                Nuestro cliente, busca lanzar su nuevo sitio de noticias llamado “TheRialNews” donde en forma satírica informa a sus usuarios de actualidad.</p>
-              <a href="https://github.com/camilaOlivaresR/TheRialNews" target="_blank" class="btn border">Github</a>
-            </div> */}
-          </div>
-        </div>
+              ))}
       </div>
     </div>
 

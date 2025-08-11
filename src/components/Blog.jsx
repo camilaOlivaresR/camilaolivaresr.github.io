760a2942b9b48cd4c1fd82bb703dd1a9a3d187f8
cami <cami@localhost.localdomain>
2025-08-11 15:14:30 -0400

    test Post1

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import ada from './img/ada.jpeg';
+
 
 
 
@@ -7,7 +7,7 @@ const Blog = () => {
   const [data, setData] = useState(null);
 
  useEffect(() => {
-    fetch('/data.json')
+    fetch('data.json')
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error('Error fetching data:', error));
@@ -23,23 +23,18 @@ const Blog = () => {
         <h2>Blog</h2>
       </div>
       <div class="row">
-         
         {data.posts.map(post => (
-        
-  
-     
         <div class="col-md-4 p-2" >
           <div class="card h-100">
             <div class="card" >
               <img src={post.image} class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">key={post.id},</h5>
-                <p class="card-text">{post.title}.</p>
+                <h5 class="card-title">{post.title}</h5>
+                <p class="card-text">{post.description}</p>
               </div>
 
               <div class="card-body">
                 <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
               </div>
             </div>
           </div>
